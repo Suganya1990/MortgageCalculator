1e1123a9bd845fe17724b7febb28d98865e6ec9b
Suganya Maheswaran <43247716+Suganya1990@users.noreply.github.com>
2025-04-25 16:46:40 -0400

    added work in progress alert

--- src/app/mortage/mortage.component.ts
+++ src/app/mortage/mortage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, OnInit } from '@angular/core';
 import { MortageFormComponent } from './mortage-form/mortage-form.component';
 import { MortgageResultsComponent } from './mortgage-results/mortgage-results.component';
 
@@ -9,6 +9,17 @@ import { MortgageResultsComponent } from './mortgage-results/mortgage-results.co
   templateUrl: './mortage.component.html',
   styleUrl: './mortage.component.css'
 })
-export class MortageComponent {
+export class MortageComponent implements OnInit{
+  constructor(){
+    this.showWorkInPrgoressMessage();
+  }
+  ngOnInit(): void {
+    
+  }
+  showWorkInPrgoressMessage () {
+    afterNextRender(() => {
+      alert("This is still a work in progress");
+    });
+}
 
 }
